9b20800dd1bc60a4bad28572d1abf02e011ed6ea
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-08 16:58:13 -0300

    terminada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,82 @@
 import React, {Component} from 'react';
 
 class App extends Component {
-  render(){
+  constructor(props){
+    super(props);
+    this.state = {
+      newItem: '',
+      list:[]
+    }
+  }
+
+  updateInput(key, value){
+    //update react state
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addItem(){
+        //create item with unique id and add to list
+        const newItem = {
+          id: 1+ Math.random(),
+          value: this.state.newItem.slice()
+      };
+      //copy of current list of items
+
+      const list = [...this.state.list];
+
+      // addd new item to the list 
+
+      list.push(newItem);
+
+      //update state with new list and reset newItem input
+      this.setState({
+        list,
+        newItem:""
+      });
+
+}
+
+  deleteItem(id){
+    //copy current list of items
+    const list = [...this.state.list];
+    //filter out the item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({list: updatedList});
+  }
+
+
+
+  render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className='App' >
+        <div>
+          Agregar una nueva tarea:
+          <br/>
+          <input type='text' 
+          placeholder='Nueva tarea'
+          value={this.state.newItem}
+          onChange= {e => this.updateInput('newItem', e.target.value)}
+          />
+          <button
+          onClick={() => this.addItem()}
+          >
+            Agregar tarea
+          </button>
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key ={item.id}> 
+                  {item.value}
+                  <button onClick={() => this.deleteItem(item.id)} >
+                    Eliminar
+                  </button>
+                </li>
+              )
+            } )}
+          </ul>
+        </div>
       </div>
     );
   }
